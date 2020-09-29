11cceab58c604cc99ab64a7d80b06b8421db3785
Wesley Araujo <wesparaestudos@gmail.com>
2020-09-29 12:46:02 -0300

    Projeto Urna Eletônica

---
+++ js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '12345',
+                nome: 'Bolt',
+                partido: 'Corredores',
+                fotos: [
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '54321',
+                nome: 'Joana',
+                partido: 'Dançarinas',
+                fotos: [
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero:'99',
+                nome:'Myke',
+                partido:'Atletas',
+                vice: 'Leo',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero:'84',
+                nome:'Lagertha',
+                partido:'Vikings',
+                vice: 'Zul',
+                fotos:[
+                    {url:'lagertha.jpg', legenda: 'Prefeito'},
+                    {url:'ragnar.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
